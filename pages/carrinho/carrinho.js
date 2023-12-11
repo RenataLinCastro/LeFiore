ca8ac2ee79976106ca94cf26b16972f7b805a358
Renata de Castro Linhares <renata.lincastro@gmail.com>
2023-12-11 17:47:05 -0300

    ajustes
    ajustes

--- pages/carrinho/carrinho.js
+++ pages/carrinho/carrinho.js
@@ -1,7 +1,28 @@
-var btnComprar = $(`#btnComprar`)
-var valor = 300
-var total = valor * $(`#qntProduto`)
+// var btnComprar = $(`#btnComprar`)
+// var valor = 300
+// var total = valor * $(`#qntProduto`)
 
-btnComprar.on(`click`, function(){
-    alert(`O total foi de R$${total}`)
-});
+class Cadastro {
+    constructor(nome, email, senha){
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+}
+
+var cadastroBtn = document.querySelector("#btnCadastro");
+var listarBtn = document.querySelector("#listarBtn");
+cadastroBtn.addEventListener("click", cadastrar);
+listarBtn.addEventListener("click", listar)
+
+function cadastrar(){
+    let nomeFilme = document.querySelector("#nomeFilme").value;
+    let descricao = document.querySelector("#descricao").value;
+    let data = document.querySelector("#data").value;
+    let categ = document.querySelector("#categ").value;
+    let msgCadastro = document.querySelector("#msgCadastro");
+
+
+    filmes.push(new Filme(nomeFilme, descricao, data, categ));
+    msgCadastro.innerHTML = `<h5 class="sucesso">Filme Cadastrado</h5>`
+}
---
+++ pages/login e cadastro/cadastro/cadastro.js
@@ -0,0 +1,18 @@
+// var cadastroBtn = document.querySelector("#btnCadastro");
+// var listarBtn = document.querySelector("#listarBtn");
+// cadastroBtn.addEventListener("click", cadastrar);
+// listarBtn.addEventListener("click", listar)
+
+// function cadastrar(){
+//     let nomeFilme = document.querySelector("#nomeFilme").value;
+//     let descricao = document.querySelector("#descricao").value;
+//     let data = document.querySelector("#data").value;
+//     let categ = document.querySelector("#categ").value;
+//     let msgCadastro = document.querySelector("#msgCadastro");
+
+
+//     filmes.push(new Filme(nomeFilme, descricao, data, categ));
+//     msgCadastro.innerHTML = `<h5 class="sucesso">Filme Cadastrado</h5>`
+// }
+
+var btnCadastro = document.querySelector("#cadastroBtn");
